207b2d5ce5d43b28a21995b8ecb5f905bebcc06f
Katherine Kent <k4tiek3nt1981@gmail.com>
2022-11-14 17:11:32 -0700

    convert numbers from metric to imperial units
    Updated api.js and convertHandler.js to start building out the conversion process

--- api.js
+++ api.js
@@ -1,10 +1,22 @@
 'use strict';
-
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
-
+var expect = require('chai').expect;
+var ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
-  let convertHandler = new ConvertHandler();
+  var convertHandler = new ConvertHandler();
+  app.route('/api/convert')
+    .get(function (req, res){
+      var input = req.query.input;
+      var index = input.search(/[a-zA-Z]/);
+      var initNum = convertHandler.getNum(input, index);
+      var initUnit = convertHandler.getUnit(input, index);
+      var returnNum = convertHandler.convert(initNum, initUnit);
+      var returnUnit = convertHandler.getReturnUnit(initUnit);
+      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
 
+      console.log(returnNum);
+
+      //res.json
+    });
+    
 };
--- convertHandler.js
+++ convertHandler.js
@@ -1,13 +1,13 @@
 function ConvertHandler() {
   
-  this.getNum = function(input) {
-    let result;
+  this.getNum = function(input, index) {
+    let result = input.slice(0, index);
     
     return result;
   };
   
-  this.getUnit = function(input) {
-    let result;
+  this.getUnit = function(input, index) {
+    let result = input.slice(index, input.length);
     
     return result;
   };
@@ -26,9 +26,37 @@ function ConvertHandler() {
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
+
+    //added backwards conversion from l to gal
+    const lToGal = 0.26417;
+    
     const lbsToKg = 0.453592;
+
+    //added backwards conversion from kg to lbs(pounds)
+    const kgToLbs = 2.20462;
+    
     const miToKm = 1.60934;
+
+    //added backwards conversion from km to mi
+    const kmToMi = 0.62137;
+    
     let result;
+
+    if(initUnit === 'gal') {
+      result = initNum * galToL;
+    } else if (initUnit === 'lbs') {
+      result = initNum * lbsToKg;
+    } else if (initUnit === 'mi') {
+      result = initNum * miToKm;
+    } else if (initUnit === 'l') {
+      result = initNum * lToGal;
+    } else if (initUnit === 'kg') {
+      result = initNum * kgToLbs;
+    } else if (initUnit === 'km') {
+      result = initNum * kmToMi;    
+    } else {
+      result = 'invalid';
+    }
     
     return result;
   };
